2a814d3469708e43a37526e5aa1be6a57244b538
Jeremy Zhang <jkzhang@uw.edu>
2020-04-14 18:19:30 -0700

    Regex based testing badwords

--- Anankos/bad_words.py
+++ Anankos/bad_words.py
@@ -5,7 +5,11 @@ class BadWords:
     def __init__(self, client, bad_words):
         self.client = client
         self.bad_words = bad_words
-        self.pattern = re.compile('[\W_]+')
+        self.re_bad_words = []
+        for bad in self.bad_words:
+            self.re_bad_words.append(
+                re.compile(bad, re.IGNORECASE + re.MULTILINE + re.DOTALL)
+            )
 
     async def on_message(self, message):
         await self.handle_bad_words(message)
@@ -19,9 +23,8 @@ class BadWords:
         if message.author.permissions_in(message.channel).manage_messages or not message.guild.me.permissions_in(message.channel).manage_messages:
             return
         delete = False
-        content = self.pattern.sub('', message.content.lower())
-        for bad in self.bad_words:
-            if bad.lower() in content:
+        for bad in self.re_bad_words:
+            if bad.match(message.content):
                 delete = True
                 break
         if delete:
