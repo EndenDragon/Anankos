8432b8c8882fd31a07ef67e850309d31125587ad
Jeremy Zhang <jkzhang@uw.edu>
2024-01-03 22:36:35 -0800

    Ensure all conditional for image embed supression

--- Anankos/image_embed.py
+++ Anankos/image_embed.py
@@ -58,7 +58,7 @@ class ImageEmbed:
         self.ready.clear()
         urls = self.extractor.find_urls(message.content, True)
         urls = [url for url in urls if self.filter_link(url, message.content)]
-        if (("twitter" in line or "pixiv" in line) for line in urls) and not force_ignore_embeds:
+        if all(("twitter" in line or "pixiv" in line) for line in urls) and not force_ignore_embeds:
             self.forced_embeds.append(message)
             if len(message.embeds):
                 await message.edit(suppress=True)
@@ -141,7 +141,9 @@ class ImageEmbed:
         return message_content.count("<" + url + ">") < message_content.count(url)
 
     async def get_twitter_embed(self, url, message, force_ignore_embeds):
-        url = url.replace("mobile.twitter.com", "twitter.com").replace("fxtwitter.com", "twitter.com").replace("vxtwitter.com", "twitter.com")
+        url = url.replace("mobile.twitter.com", "twitter.com")
+        if "fxtwitter" in url.lower() or "vxtwitter" in url.lower():
+            return None
         twitter_id = self.twitter_pattern.search(url)
         if not twitter_id:
             return None
